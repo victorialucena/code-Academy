58b1444fe813d9af77008484a5a05a1755c4477d
Victoria Lucena <lucenavictoria0@gmail.com>
2023-01-19 03:37:02 -0300

    data js

---
+++ codeacademy-react/data/data.js
@@ -0,0 +1,17 @@
+export const infos = [
+  {
+    id: 0,
+    image: '../../images/pedro.jpeg',
+    alt: 'a definir',
+    nome: 'Pedro Sergio Borges Ribeiro',
+    idade: 24,
+    profisao: 'Piloto de Avião',
+    comida: 'Lasanha',
+    musicas: ['Paradise - ColdPlay e Tá Rocheda', 'Barões da Pisadinha'],
+    linguagens: ['AWS', 'TypeScript', 'Golang']
+    },
+    {
+        id: 1,
+        
+    }
+]
