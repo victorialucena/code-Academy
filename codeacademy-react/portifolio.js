26179d8655ec1f2041ddbe63a80284c8d43ca8b2
Victoria Lucena <lucenavictoria0@gmail.com>
2023-01-19 03:38:03 -0300

    import banner

---
+++ codeacademy-react/portifolio.js
@@ -0,0 +1,14 @@
+import Banner from './src/components/banner'
+import Header from './src/components/header'
+import './reset.css'
+import './style.css'
+
+function Portfolio() {
+  return (
+    <div className="div-portifolio">
+      <Header />
+      <Banner />
+    </div>
+  )
+}
+export default Portfolio
