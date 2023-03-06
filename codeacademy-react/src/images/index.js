be8c574a2e1f251b58bcfcddd728b9b16854b178
victorialucena <lucenavictoria0@gmail.com>
2023-03-05 23:52:56 -0300

    fix: adding index from images

---
+++ codeacademy-react/src/images/index.js
@@ -0,0 +1,17 @@
+export { default as aninha } from "./aninha.jpeg"
+
+export {default as calixto} from "./calixto.png"
+
+export { default as pedro } from "./pedro.jpeg"
+
+export {default as robson} from "./robson.jpeg"
+
+export {default as tenorio} from "./tenorio.png"
+
+export {default as vic} from "./vic.jpeg"
+
+export {default as logoIq} from "./logo-iq.svg"
+
+export {default as codeAcademy} from "./codeacademy.jpeg"
+
+export {default as redLogo} from "./redlogo.png"
