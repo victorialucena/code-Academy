bd2a80f9cd9b430f8d369cd82a2ad6a5ba38f260
victorialucena <lucenavictoria0@gmail.com>
2023-03-05 23:57:02 -0300

    refatoring: changing images path and alt

--- codeacademy-react/src/components/banner/index.js
+++ codeacademy-react/src/components/banner/index.js
@@ -1,10 +1,10 @@
 import './style.css'
-import codeAcademy from '../../images/codeacademy.jpeg'
+import { codeAcademy } from '../../images/index'
 
 export default function Banner() {
   return (
     <div className="div-banner">
-      <img className="codeacademy-img" src={codeAcademy} alt="o time da codeacademy, contendo sete pessoas, sendo duas mulheres e cinco homens" />
+      <img className="codeacademy-img" src={codeAcademy} alt="o time da codeacademy, contendo seis pessoas, sendo duas mulheres e quatro homens" />
     </div>
   )
 }
--- codeacademy-react/src/components/header/index.js
+++ codeacademy-react/src/components/header/index.js
@@ -1,13 +1,13 @@
 import './style.css'
-import redLogo from '../../images/redlogo.png'
-import iqLogo from '../../images/logo-iq.svg'
+import { logoIq } from '../../images/index'
+import { redLogo } from '../../images/index'
 
 export default function Header() {
   return (
     <div className="div-header">
       <img className="logo-rv" src={redLogo} alt="logo da red ventures" />
       <h1 className="title-header">CodeAcademy</h1>
-      <img className="logo-iq" src={iqLogo} alt="logo do iq" />
+      <img className="logo-iq" src={logoIq} alt="logo do iq" />
     </div>
   )
 }
